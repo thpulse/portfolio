1749053ac5db104ad2131a98b823f0d5dd20bc3b
soojung <soojung@sj>
2017-01-17 14:40:26 +0900

    인규base + 수정 push 0117 14시 40분

--- src/main/java/kitri/performinfo/prfplace/dao/PrfplaceDAO.java
+++
@@ -1,7 +0,0 @@
-package kitri.performinfo.prfplace.dao;
-
-public interface PrfplaceDAO {
-
-	void Add_Prfplace();
-
-}
--- src/main/java/kitri/performinfo/prfplace/service/PrfplaceService.java
+++
@@ -1,7 +0,0 @@
-package kitri.performinfo.prfplace.service;
-
-public interface PrfplaceService {
-
-	void Add_Prfplace();
-
-}
