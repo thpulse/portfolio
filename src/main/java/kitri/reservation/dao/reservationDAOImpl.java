b0fda5aad104c0985f330a0c6f12ccc5b21bea1e
kimtaehyung <thpulsek@naver.com>
2017-01-23 11:18:53 +0900

    김태형 예약부분 거의 완성 - ajax로 공연 시간표 마우스오버하면 공연 남은좌석뜨게하기

--- src/main/java/kitri/reservation/dao/reservationDAOImpl.java
+++ src/main/java/kitri/reservation/dao/reservationDAOImpl.java
@@ -1,17 +1,29 @@
 package kitri.reservation.dao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 import kitri.performinfo.performance.dto.PerformanceDTO;
 import kitri.performinfo.prfplace.dto.PrfplaceDTO;
+import kitri.reservation.service.reservationService;
+import kitri.reservation.vo.reservationDetailVO;
+import kitri.reservation.vo.reservationVO;
 @Repository("reservationdao")
 public class reservationDAOImpl implements reservationDAO{
 	@Autowired
 	SqlSession sqlSession;
+	@Autowired
+	@Qualifier("reservationservice")
+	reservationService reservService;
 	
 	@Override
 	public List<PrfplaceDTO> call_hall_bySido(String sido) {
@@ -31,6 +43,35 @@ public class reservationDAOImpl implements reservationDAO{
 	@Override
 	public PerformanceDTO perform_detail(String prfid) {
 		return sqlSession.selectOne("kitri.reservation.perform_detail", prfid);
+	}
+
+	@Override
+	public int insert_reservation(reservationVO vo) {
+		//예매 테이블		
+		return sqlSession.insert("kitri.reservation.insert_reservation", vo);
+			
+	}
+
+	@Override
+	public int insert_reservationDeatil(reservationDetailVO vo) {		
+		return sqlSession.insert("kitri.reservation.insert_reservation_detail",vo);
+	}
+
+	@Override
+	public List<String> reservation_chk_byTime(reservationVO vo) {
+		return sqlSession.selectList("kitri.reservation.reservation_chk_byTime", vo);
+	}
+
+	@Override
+	public List<String> reservation_chk_byTime_detail(reservationVO vo) {
+		return sqlSession.selectList("kitri.reservation.reservation_chk_byTime_detail",vo);
+	}
+
+	@Override
+	public int preChk_byTime_detail(reservationVO vo) {
+		// TODO Auto-generated method stub
+		return 0;
 	}	
+	
 
 }
--- src/main/java/kitri/reservation/vo/reservationVO.java
+++ src/main/java/kitri/reservation/vo/reservationVO.java
@@ -2,27 +2,47 @@ package kitri.reservation.vo;
 
 public class reservationVO {
 	String reserv_num;
+	String reserv_date;
+	String seat;
 	String user_id;
 	String prf_id;
 	String prf_day;
+	String prf_day_yo;
 	String prf_starttime;
-	String def_flg;
-	public reservationVO(){}
-	public reservationVO(String reserv_num, String user_id, String prf_id, String prf_day, String prf_starttime,
-			String def_flg) {
+	String del_flg;
+	public reservationVO() {
+		super();
+	}
+	public reservationVO(String reserv_num, String reserv_date, String seat, String user_id, String prf_id,
+			String prf_day, String prf_day_yo, String prf_starttime, String del_flg) {
 		super();
 		this.reserv_num = reserv_num;
+		this.reserv_date = reserv_date;
+		this.seat = seat;
 		this.user_id = user_id;
 		this.prf_id = prf_id;
 		this.prf_day = prf_day;
+		this.prf_day_yo = prf_day_yo;
 		this.prf_starttime = prf_starttime;
-		this.def_flg = def_flg;
+		this.del_flg = del_flg;
 	}
 	public String getReserv_num() {
 		return reserv_num;
 	}
 	public void setReserv_num(String reserv_num) {
 		this.reserv_num = reserv_num;
+	}
+	public String getReserv_date() {
+		return reserv_date;
+	}
+	public void setReserv_date(String reserv_date) {
+		this.reserv_date = reserv_date;
+	}
+	public String getSeat() {
+		return seat;
+	}
+	public void setSeat(String seat) {
+		this.seat = seat;
 	}
 	public String getUser_id() {
 		return user_id;
@@ -42,18 +62,35 @@ public class reservationVO {
 	public void setPrf_day(String prf_day) {
 		this.prf_day = prf_day;
 	}
+	public String getPrf_day_yo() {
+		return prf_day_yo;
+	}
+	public void setPrf_day_yo(String prf_day_yo) {
+		this.prf_day_yo = prf_day_yo;
+	}
 	public String getPrf_starttime() {
 		return prf_starttime;
 	}
 	public void setPrf_starttime(String prf_starttime) {
 		this.prf_starttime = prf_starttime;
 	}
-	public String getDef_flg() {
-		return def_flg;
+	public String getDel_flg() {
+		return del_flg;
 	}
-	public void setDef_flg(String def_flg) {
-		this.def_flg = def_flg;
+	public void setDel_flg(String del_flg) {
+		this.del_flg = del_flg;
 	}
+	@Override
+	public String toString() {
+		return "reservationVO [reserv_num=" + reserv_num + ", reserv_date=" + reserv_date + ", seat=" + seat
+				+ ", user_id=" + user_id + ", prf_id=" + prf_id + ", prf_day=" + prf_day + ", prf_day_yo=" + prf_day_yo
+				+ ", prf_starttime=" + prf_starttime + ", del_flg=" + del_flg + "]";
+	}	
 	
-
+	
+	
+	
+	
+	
+	
 }
